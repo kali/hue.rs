61e11c4945fc6dff9a0efa33604b73d76e06647d
Mathieu Poumeyrol <kali@zoy.org>
2015-01-20 20:41:14 +0100

    start on ligths

---
+++ src/bin/hue_get_all_lights.rs
@@ -0,0 +1,15 @@
+#![allow(unstable)]
+extern crate hue;
+use std::os;
+use hue::errors::HueError;
+
+fn main() {
+    let args = os::args();
+    if args.len() < 2 {
+        println!("usage : {} <username>", args[0]);
+        return
+    }
+    let bridge = ::hue::bridge::Bridge::discover_required().with_user(args[1].clone());
+    let lights = bridge.get_all_lights();
+    println!("{:?}", lights);
+}
--- src/bridge.rs
+++ src/bridge.rs
@@ -1,27 +1,36 @@
 use hyper::Client;
 use hyper::client::Body;
+use hyper::method::Method;
+use hyper::client::response::Response;
+use hyper::method::Method::{Post, Get, Put};
 use disco;
 use rustc_serialize::json;
 use rustc_serialize::json::Json;
 use errors::HueError;
 use errors::AppError;
+use std::collections::BTreeMap;
 
 #[derive(Show)]
 pub struct Bridge {
-    ip: String
+    ip: String,
+    username: Option<String>,
 }
 
 impl Bridge {
     #[allow(dead_code)]
     pub fn discover() -> Option<Bridge> {
-        disco::discover_hue_bridge().ok().map( |i| Bridge{ ip:i } )
+        disco::discover_hue_bridge().ok().map( |i| Bridge{ ip:i, username:None } )
     }
 
     pub fn discover_required() -> Bridge {
-        Bridge::discover().unwrap_or_else( || panic!("No bridge found!"))
+        Bridge::discover().unwrap_or_else( || panic!("No bridge found!") )
     }
 
-    pub fn register_user(&self, devicetype:&str, username:&str) -> Result<String,HueError> {
+    pub fn with_user(self, username:String) -> Bridge {
+        Bridge{ username: Some(username), ..self }
+    }
+
+    pub fn register_user(&self, devicetype:&str, username:&str) -> Result<Json,HueError> {
         if username.len() < 10 || username.len() > 40 {
             return HueError::wrap("username must be between 10 and 40 characters")
         }
@@ -34,20 +43,26 @@ impl Bridge {
             devicetype:devicetype.to_string(),
             username:username.to_string()
         };
-        //println!("{}", json::encode(&obtain));
-        let b = try!(self.post("/api", json::encode(&obtain)));
-        Ok(b.to_string())
+        let body = json::encode(&obtain);
+        let mut client = Client::new();
+        let url = format!("http://{}/api", self.ip);
+        let mut resp = try!(client.post(url.as_slice())
+            .body(Body::BufBody(body.as_bytes(), body.as_bytes().len())).send());
+        self.parse_write_resp(&mut resp)
     }
 
-    pub fn post(&self, path:&str, body:String) -> Result<Json,HueError> {
+    pub fn get_all_lights(&self) -> Result<BTreeMap<String,Json>,HueError> {
+        let url = format!("http://{}/api/{}/lights",
+            self.ip, self.username.clone().unwrap());
         let mut client = Client::new();
-        let url = format!("http://{}{}", self.ip, path);
-        //println!("url: {}", url);
-        let mut resp = try!(client.post(url.as_slice())
-            .body(Body::BufBody(body.as_bytes(), body.as_bytes().len()))
-            .send());
+        let mut resp = try!(client.get(url.as_slice()).send());
         let json = try!(::tools::from_reader(&mut resp));
-        //println!("{}", json);
+        Ok(json.as_object().unwrap().clone())
+    }
+
+    fn parse_write_resp(&self, resp:&mut Response) -> Result<Json,HueError> {
+        let json = try!(::tools::from_reader(resp));
+        println!("{}", json);
         let objects = try!(json.as_array()
             .ok_or(HueError::Error("expected array".to_string())));
         if objects.len() == 0 {
