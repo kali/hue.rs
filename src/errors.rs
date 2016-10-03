86280f6f228bdf2a62ce21ef017891d4fa29396b
LFalch <lucas@wasd.dk>
2016-10-03 17:38:11 +0200

    Complete `BridgeError`
    Don't use transmute for converting from u16

--- src/errors.rs
+++ src/errors.rs
@@ -28,39 +28,98 @@ pub enum HueError {
     ParseIntError(ParseIntError)
 }
 
-/// All errors defined in [http://www.developers.meethue.com/documentation/error-messages]
-#[repr(u16)]
-#[allow(missing_docs)]
-#[derive(Debug, Copy, Clone)]
-pub enum BridgeError{
-    // Generic Errors
-    UnauthorizedUser = 1,
-    BodyContainsInvalidJSON = 2,
-    ResourceNotAvailable = 3,
-    MethodNotAvailableForResource = 4,
-    MissingParametersInBody = 5,
-    ParameterNotAvailable = 6,
-    InvalidValueForParameter = 7,
-    ParameterIsNotModifiable = 8,
-    TooManyItemsInList = 11,
-    ProtalConnectionRequired = 12,
-    InternalError = 901,
-
-    // Command Specific Errors
-    LinkButtonNotPressed = 101,
-    DHCPCannotBeDisabled = 110,
-    InvalidUpdateState = 111,
-    // TODO add the rest of the command specific errors
-    Other
+macro_rules! error_enum {
+    (
+        $(#[$meta:meta])*
+        pub enum $name:ident{
+            $($err:ident = $n:expr),+;
+            $other:ident
+        }
+    ) => (
+        $(#[$meta])*
+        pub enum $name{
+            $($err = $n,)+
+            $other
+        }
+        impl From<u16> for $name{
+            fn from(n: u16) -> Self{
+                match n {
+                    $($n => $name::$err,)+
+                    _ => $name::$other
+                }
+            }
+        }
+    );
 }
 
-use std::mem::transmute;
+error_enum!{
+    /// All errors defined in http://www.developers.meethue.com/documentation/error-messages
+    #[repr(u16)]
+    #[allow(missing_docs)]
+    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+    pub enum BridgeError{
+        // Generic Errors
+        UnauthorizedUser = 1,
+        BodyContainsInvalidJson = 2,
+        ResourceNotAvailable = 3,
+        MethodNotAvailableForResource = 4,
+        MissingParametersInBody = 5,
+        ParameterNotAvailable = 6,
+        InvalidValueForParameter = 7,
+        ParameterIsNotModifiable = 8,
+        TooManyItemsInList = 11,
+        ProtalConnectionRequired = 12,
+        InternalError = 901,
+
+        // Command Specific Errors
+        LinkButtonNotPressed = 101,
+        DHCPCannotBeDisabled = 110,
+        InvalidUpdateState = 111,
+        DeviceIsSetToOff = 201,
+        GroupCouldNotBeCreatedGroupFull = 301,
+        DeviceCouldNotBeAddedGroupFull = 302,
+        DeviceIsUnreachable = 304,
+        UpdateOrDeleteGroupOfThisTypeNotAllowed = 305,
+        LightAlreadyUsed = 306,
+        SceneCouldNotBeCreated = 401,
+        SceneCouldNotBeCreatedBufferFull = 402,
+        SceneCouldNotBeRemoved = 403,
+        NotAllowedToCreateSensorType = 501,
+        SensorListIsFull = 502,
+        RuleEngineFull = 601,
+        ConditionError = 607,
+        ActionError = 608,
+        UnableToActivae = 609,
+        ScheduleListIsFull = 701,
+        ScheduleTimezoneNotValid = 702,
+        ScheduleCannotSetTimeAndLocalTime = 703,
+        CannotCreateSchedule = 704,
+        CannotEnableScheduleTimeInPast = 705,
+        CommandError = 706,
+        SourceModelInvalid = 801,
+        SourceFactoryNew = 802,
+        InvalidState = 803;
+        Other
+    }
+}
+
+#[test]
+fn bride_errors() {
+    use self::BridgeError::*;
+
+    assert_eq!(BridgeError::from(101), LinkButtonNotPressed);
+    assert_eq!(BridgeError::from(0), Other);
+    assert_eq!(BridgeError::from(51234), Other);
+    assert_eq!(BridgeError::from(4), MethodNotAvailableForResource);
+    assert_eq!(SceneCouldNotBeRemoved as u16, 403);
+    assert_eq!(InternalError as u16, 901);
+}
 
 impl From<::hue::Error> for HueError {
     fn from(::hue::Error{address, code,..}: ::hue::Error) -> Self {
         HueError::BridgeError{
             address: address,
-            error: unsafe{transmute(code)}
+            error: From::from(code)
         }
     }
 }
