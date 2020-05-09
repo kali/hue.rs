1cff993509a4d9b5981e41d6abee7928c3293c60
Mathieu Poumeyrol <kali@zoy.org>
2020-05-09 22:20:49 +0200

    fix disco url

--- src/disco.rs
+++ src/disco.rs
@@ -1,22 +1,21 @@
+use crate::*;
 use reqwest;
 use serde_json::*;
-use crate::*;
 
 pub fn discover_hue_bridge() -> HueResult<String> {
-    let objects: Vec<Map<String, Value>> = reqwest::blocking::get("https://www.meethue.com/api/nupnp")?
-        .json()?;
+    let objects: Vec<Map<String, Value>> =
+        reqwest::blocking::get("https://discovery.meethue.com/")?.json()?;
 
     if objects.len() == 0 {
         Err("expected non-empty array")?
     }
     let ref object = objects[0];
 
-    let ip = object.get("internalipaddress").ok_or(
-        "Expected internalipaddress",
-    )?;
-    Ok(
-        ip.as_str()
-            .ok_or("expect a string in internalipaddress")?
-            .to_string(),
-    )
+    let ip = object
+        .get("internalipaddress")
+        .ok_or("Expected internalipaddress")?;
+    Ok(ip
+        .as_str()
+        .ok_or("expect a string in internalipaddress")?
+        .to_string())
 }
